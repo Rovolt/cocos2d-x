7539aad889eeb67b50c18c8f471273dd15103b6b
Andriy Chaika <andriy+en@chaika.in.ua>
2013-02-15 21:45:09 +0200

    win8

--- cocos2dx/CCFileUtils_win8_metro.cpp
+++ cocos2dx/CCFileUtils_win8_metro.cpp
@@ -495,33 +495,7 @@ const char* CCFileUtils::fullPathFromRelativePath(const char *pszRelativePath)
 		pRet->m_sString = pszRelativePath;
 	}
 
-	//#if (CC_IS_RETINA_DISPLAY_SUPPORTED)
-	//    if (CC_CONTENT_SCALE_FACTOR() != 1.0f)
-	//    {
-	//        std::string hiRes = pRet->m_sString.c_str();
-	//        std::string::size_type pos = hiRes.find_last_of("/\\");
-	//        std::string::size_type dotPos = hiRes.find_last_of(".");
-	//        
-	//        if (std::string::npos != dotPos && dotPos > pos)
-	//        {
-	//            hiRes.insert(dotPos, CC_RETINA_DISPLAY_FILENAME_SUFFIX);
-	//        }
-	//        else
-	//        {
-	//            hiRes.append(CC_RETINA_DISPLAY_FILENAME_SUFFIX);
-	//        }
-	//        DWORD attrib = GetFileAttributesA(hiRes.c_str());
-	//        
-	//        if (attrib != INVALID_FILE_ATTRIBUTES && ! (FILE_ATTRIBUTE_DIRECTORY & attrib))
-	//        {
-	//            pRet->m_sString.swap(hiRes);
-	//        }
-	//    }
-	//#endif
-	/*if (pResolutionType)
-	{
-	*pResolutionType = kCCResolutioniPhone;
-	}*/
+
 	return pRet->m_sString.c_str();
 }
 
